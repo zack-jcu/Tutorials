74a7c999c65109e73af05de53443fe9ddc8e5a7a
Zackary Wright <zackary.wright@my.jcu.edu.au>
2019-09-04 11:54:16 +1000

    Add improved output

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -4,14 +4,20 @@ NUMBERS_PER_LINE = 6
 
 
 def main():
-    picks = 0
-    number_of_picks = int(input("How many quick picks: "))
-    while picks != number_of_picks:
+    quick_picks = int(input("How many quick picks? "))
+    for line in range(quick_picks):
+        numbers_generated = calculate_row_numbers()
+        numbers_generated.sort()
+        print(numbers_generated)
 
 
-def lines():
-    while i in range(1, NUMBERS_PER_LINE):
+def calculate_row_numbers():
+    numbers = []
+    for i in range(NUMBERS_PER_LINE):
         number = random.randint(1, 45)
+        numbers.append(number)
+    return numbers
 
 
 main()
+
